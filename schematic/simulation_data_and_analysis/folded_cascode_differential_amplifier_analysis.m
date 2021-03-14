5e306d5526c42163556b56711a43012d4d89de97
Thomas <thomas.jagielski@yahoo.com>
2021-03-13 21:05:51 -0500

    Add matlab and plots

--- schematic/simulation_data_and_analysis/folded_cascode_differential_amplifier_analysis.m
+++ schematic/simulation_data_and_analysis/folded_cascode_differential_amplifier_analysis.m
@@ -7,11 +7,25 @@
 vtc_data = csvread('./folded_cascode_vtc.csv',1);
 
 % Separate Data
-vtc_0_1 = vtc_data(1:181,:);
-vtc_0_3 = vtc_data(182:362,:);
-vtc_0_5 = vtc_data(363:543,:);
-vtc_0_7 = vtc_data(544:724,:);
-vtc_0_9 = vtc_data(725:end,:);
+vtc_0_1 = vtc_data(1:18001,:);
+vtc_0_3 = vtc_data(18002:36003,:);
+vtc_0_5 = vtc_data(36004:54005,:);
+vtc_0_7 = vtc_data(54006:72007,:);
+vtc_0_9 = vtc_data(72008:end,:);
+
+% Lines of best fit
+v_in = linspace(0,1.8);
+
+vtc_0_1_fit_params = polyfit(vtc_data(998:1002,2),vtc_0_1(998:1002,1),1);
+vtc_0_1_fit_line = vtc_0_1_fit_params(1) * v_in + vtc_0_1_fit_params(2);
+vtc_0_3_fit_params = polyfit(vtc_0_3(2997:3001,2),vtc_0_3(2997:3001,1),1);
+vtc_0_3_fit_line = vtc_0_3_fit_params(1) * v_in + vtc_0_3_fit_params(2);
+vtc_0_5_fit_params = polyfit(vtc_0_5(4995:5002,2),vtc_0_5(4995:5002,1),1);
+vtc_0_5_fit_line = vtc_0_5_fit_params(1) * v_in + vtc_0_5_fit_params(2);
+vtc_0_7_fit_params = polyfit(vtc_0_7(6993:7001,2),vtc_0_7(6993:7001,1),1);
+vtc_0_7_fit_line = vtc_0_7_fit_params(1) * v_in + vtc_0_7_fit_params(2);
+vtc_0_9_fit_params = polyfit(vtc_0_9(8970:9012,2),vtc_0_9(8970:9012,1),1);
+vtc_0_9_fit_line = vtc_0_9_fit_params(1) * v_in + vtc_0_9_fit_params(2);
 
 % Plot Data
 figure
@@ -21,12 +35,20 @@ plot(vtc_0_3(:,2), vtc_0_3(:,1),'.')
 plot(vtc_0_5(:,2), vtc_0_5(:,1),'.')
 plot(vtc_0_7(:,2), vtc_0_7(:,1),'.')
 plot(vtc_0_9(:,2), vtc_0_9(:,1),'.')
+plot(v_in,vtc_0_1_fit_line)
+plot(v_in,vtc_0_3_fit_line)
+plot(v_in,vtc_0_5_fit_line)
+plot(v_in,vtc_0_7_fit_line)
+plot(v_in,vtc_0_9_fit_line)
 hold off
+axis([0 1.8 0 1.8])
 title('Voltage Transfer Characteristics - Folded Cascode Differential Amplifier')
 xlabel('V_{1}=V_{in} [V]')
 ylabel('V_{out} [V]')
 legend('V_{2} = 0.1 V','V_{2} = 0.3 V','V_{2} = 0.5 V',...
-    'V_{2} = 0.7 V','V_{2} = 0.9 V', 'location', 'Southeast')
+    'V_{2} = 0.7 V','V_{2} = 0.9 V', 'Fit V_{2} = 0.1 V',...
+    'Fit V_{2} = 0.3 V','Fit V_{2} = 0.5 V','Fit V_{2} = 0.7 V',...
+    'Fit V_{2} = 0.9 V','location', 'Southeast')
 
 %% Voltage-to-Current Transfer Chracteristics
 % order of columns = [Iout, V1, V2, Vout]
@@ -53,7 +75,8 @@ ylabel('I_{out} [A]')
 hold on
 plot(voltage_to_current_input_diff,best_fit_line_voltage_to_current)
 hold off
-%legend('Simulation Data','Line of Best Fit','Location','Southeast')
+legend('Simulation Data','Line of Best Fit','Location','Southeast')
+axis([-0.2 0.2 -0.5e-6 0.5e-6])
 
 %% Loopgain
 % order of columns = Frequency [Hz], Magnitude [dB], Phase [Degrees]
